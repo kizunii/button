2fa49d7c3320550f46377c85bcd25f173b61543e
kizunii <schmittsemil@gmail.com>
2025-09-10 21:42:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const highscoreDisplay = document.getElementById('highscore');
-let highscore = 0;
 
-// Im Button-Click Event (da wo du counter++ machst) einfügen:
-if (counter > highscore) {
-  highscore = counter;
-  highscoreDisplay.textContent = "Highscore: " + highscore;
-}
