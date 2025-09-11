9ecc88586e05a0a8b2fa6fb9de918a21e973040b
kizunii <schmittsemil@gmail.com>
2025-09-11 16:57:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ let multiplier = 1
 let multiplier_cost = 25
 
 let auto_clicker = 0
-let auto_clicker = 20
+let auto_clicker_cost = 20
 
-function displayPointsAmt()
- button.innerText = "You have"+pussy_points+" Pussy Points!"
-);
+function displayPointsAmt(){
+ counterEl.innerText = "You have"+pussy_points+" Pussy Points!"
+};
 
 button.addEventListener('click',function(){
  pussy_points+=multiplier
@@ -26,7 +26,7 @@ button.addEventListener('click',function(){
 
 
      // Counter aus localStorage laden oder 0, wenn nicht vorhanden
-    let counter = parseInt(localStorage.getItem('catClickCounter')) || 0;
+    let pussy_points = parseInt(localStorage.getItem('tClickCounter')) || 0;
     counterEl.textContent = counter;
 
     // Event Listener für Button Klick
