4d312cf9df7c9b36f50afd3bb983c4dcb7ba8a86
kizunii <schmittsemil@gmail.com>
2025-09-12 12:14:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ setInterval(() => {
 }, 1000);
 
 // Hauptbutton Klick
-button.addEventListener('click', () => {
+button.addEventListener('click',function() => {
   sound.currentTime = 0;
   sound.play();
   pussy_points += multiplier;
@@ -40,7 +40,7 @@ button.addEventListener('click', () => {
 });
 
 // Upgrade Multiplier
-upgrade_button.addEventListener('click', () => {
+upgrade_button.addEventListener('click',function() => {
   if(pussy_points >= multiplier_cost){
     pussy_points -= multiplier_cost;
     multiplier_cost*=1.3
@@ -57,7 +57,7 @@ upgrade_button.addEventListener('click', () => {
 });
 
 // Kaufe AutoClicker
-auto_clicker_button.addEventListener('click', () => {
+auto_clicker_button.addEventListener('click',function() => {
   if(pussy_points >= auto_clicker_cost){
     pussy_points -= auto_clicker_cost;
     auto_clicker += 1;
