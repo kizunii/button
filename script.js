0b1231b031d162cb95433d78118749e3e63f01b7
kizunii <schmittsemil@gmail.com>
2025-09-14 17:21:45 +0200

    s

--- script.js
+++ script.js
@@ -184,14 +184,4 @@ const text = "Pussy Clicker";
     }
 
     window.onload = typeWriter;
-
-const sidebar = document.getElementById('sidebar');
-const toggleBtn = document.querySelector('.openBtn');
-
-toggleBtn.addEventListener('click', () => {
-  if(sidebar.style.width === '250px') {
-    sidebar.style.width = '0';
-  } else {
-    sidebar.style.width = '250px';
-  }
-});
+    
