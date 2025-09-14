8af08fd44542dc875f20a2d7f68a3d092a9f7e86
globusch <163164112+globusch@users.noreply.github.com>
2025-09-14 11:29:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function updateButtonTexts() {
 
 // Anzeige direkt beim Laden updaten
 function displayPointsAmt() {
-  counterEl.innerText = "You have " + roundToInt(pussy_points) + " Pussy Points!";
+  counterEl.innerText = roundToInt(pussy_points) + "Pussy Points";
 }
 
 displayPointsAmt();
